2cd2e2244f0ee05b47bf4956c6d1a4ae7fcf0751
birendramahto <60342800+birendramahto@users.noreply.github.com>
2020-07-13 21:16:51 +0530

    committed

--- lecture/lecture41/js/script.js
+++ lecture/lecture41/js/script.js
@@ -1,5 +1,15 @@
 var message="in global";
 console.log("global:message = " +message);
-//var a=function () {
-// console.log()	
-}
+
+var a=function () {
+ 	var message= "inside a";
+ 	console.log("a: message =" + message);
+
+ 	function b(){
+ 	console.log("b: message = " +message)	
+ }
+ 	b();	
+ }
+
+ 
+a();
